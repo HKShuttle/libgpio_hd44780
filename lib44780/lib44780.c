389e9a0176087ed0dd4d7a19b6c05542c1cced87
Sola <terakou1105@gmail.com>
2024-03-26 23:20:00 +0900

    WIP: Implement lib44780.c

--- lib44780/lib44780.c
+++ lib44780/lib44780.c
@@ -16,61 +16,38 @@ struct Lcd
 	char *chipname;
 };
 
-int openLcd(struct Lcd lcd, char[] chipname, int rs, int en, int d4, int d5, int d6, int d7){
-	lcd.chipname = &chipname;
-	lcd.chip = gpiod_chip_open_by_name(lcd.chipname);
-	if(!lcd.chip){
+int openLcd(struct Lcd *lcd, char *chipname, int rs, int en, int d4, int d5, int d6, int d7){
+	lcd->chipname = chipname;
+	lcd->chip = gpiod_chip_open_by_name(lcd->chipname);
+	if(!lcd->chip){
 		return 1;
 	}
 	// WIP: to catch gpiod_chip_get_line() error
-	lcd.rs = gpiod_chip_get_line(lcd.chip, rs);
-	lcd.en = gpiod_chip_get_line(lcd.chip, en);
-	lcd.d4 = gpiod_chip_get_line(lcd.chip, d4);
-	lcd.d5 = gpiod_chip_get_line(lcd.chip, d5);
-	lcd.d6 = gpiod_chip_get_line(lcd.chip, d6);
-	lcd.d7 = gpiod_chip_get_line(lcd.chip, d7);
+	lcd->rs = gpiod_chip_get_line(lcd->chip, rs);
+	lcd->en = gpiod_chip_get_line(lcd->chip, en);
+	lcd->d4 = gpiod_chip_get_line(lcd->chip, d4);
+	lcd->d5 = gpiod_chip_get_line(lcd->chip, d5);
+	lcd->d6 = gpiod_chip_get_line(lcd->chip, d6);
+	lcd->d7 = gpiod_chip_get_line(lcd->chip, d7);
+
+	gpiod_line_request_output(lcd->rs, APP, 0);
+	gpiod_line_request_output(lcd->en, APP, 0);
+	gpiod_line_request_output(lcd->d4, APP, 0);
+	gpiod_line_request_output(lcd->d5, APP, 0);
+	gpiod_line_request_output(lcd->d6, APP, 0);
+	gpiod_line_request_output(lcd->d7, APP, 0);
+
+	return 0;
 }
 
-int closeLcd(){
-
-}
-
-int main(int argc, char **argv)
-{
-	struct Pinout pin;
-	struct gpiod_chip *chip;
-
-	gpiod_line_request_output(pin.lcd_rs, APP, 0);
-	gpiod_line_request_output(pin.lcd_en, APP, 0);
-	gpiod_line_request_output(pin.lcd_d4, APP, 0);
-	gpiod_line_request_output(pin.lcd_d5, APP, 0);
-	gpiod_line_request_output(pin.lcd_d6, APP, 0);
-	gpiod_line_request_output(pin.lcd_d7, APP, 0);
-
-	initDisplay(pin);
-	sendByte(pin, 0, 0b10000000);
-	char txt[32] = "Hello World!\nHD44780 Char LCD";	
-	for (int i = 0; i < 32; i++)
-	{
-		if (txt[i] == '\0')
-		{
-			break;
-		}
-		if (txt[i] == '\n')
-		{
-			sendByte(pin, 0, 0b10000000 | 0x40); // go to next row
-			continue;
-		}
-		sendByte(pin, 1, txt[i]);
-	}
-	
-	gpiod_line_release(pin.lcd_rs);
-	gpiod_line_release(pin.lcd_en);
-	gpiod_line_release(pin.lcd_d4);
-	gpiod_line_release(pin.lcd_d5);
-	gpiod_line_release(pin.lcd_d6);
-	gpiod_line_release(pin.lcd_d7);
-	gpiod_chip_close(chip);
+int closeLcd(struct Lcd *lcd){
+	gpiod_line_release(lcd->rs);
+	gpiod_line_release(lcd->en);
+	gpiod_line_release(lcd->d4);
+	gpiod_line_release(lcd->d5);
+	gpiod_line_release(lcd->d6);
+	gpiod_line_release(lcd->d7);
+	gpiod_chip_close(lcd->chip);
 	return 0;
 }
 
@@ -82,21 +59,21 @@ void ndelay(int nanosec)
 	nanosleep(&req, NULL);	
 }
 
-void sendNibble(struct Pinout pin, int rs, int d7, int d6, int d5, int d4)
+void sendNibble(struct Lcd *lcd, int rs, int d7, int d6, int d5, int d4)
 {
-	gpiod_line_set_value(pin.lcd_rs, rs);
+	gpiod_line_set_value(lcd->rs, rs);
 	ndelay(60 * 2);
-	gpiod_line_set_value(pin.lcd_en, 1);
-	gpiod_line_set_value(pin.lcd_d4, d4);
-	gpiod_line_set_value(pin.lcd_d5, d5);
-	gpiod_line_set_value(pin.lcd_d6, d6);
-	gpiod_line_set_value(pin.lcd_d7, d7);
+	gpiod_line_set_value(lcd->en, 1);
+	gpiod_line_set_value(lcd->d4, d4);
+	gpiod_line_set_value(lcd->d5, d5);
+	gpiod_line_set_value(lcd->d6, d6);
+	gpiod_line_set_value(lcd->d7, d7);
 	ndelay(450 * 2);
-	gpiod_line_set_value(pin.lcd_en, 0);
+	gpiod_line_set_value(lcd->en, 0);
 	ndelay(1200 * 2);
 }
 
-void sendByte(struct Pinout pin, int rs, int byte)
+void sendByte(struct Lcd *lcd, int rs, int byte)
 {
 	int data[8]; // data[0]=LSB, data[7]=MSB
 	for (int i = 0; i <= 7; i++)
@@ -105,22 +82,66 @@ void sendByte(struct Pinout pin, int rs, int byte)
 		byte >>= 1;
 	}
 	
-	sendNibble(pin, rs, data[7], data[6], data[5], data[4]);
-	sendNibble(pin, rs, data[3], data[2], data[1], data[0]);	
+	sendNibble(lcd, rs, data[7], data[6], data[5], data[4]);
+	sendNibble(lcd, rs, data[3], data[2], data[1], data[0]);	
 }
 
-void initDisplay(struct Pinout pin)
+void sendCommand(struct Lcd *lcd, int byte){
+	sendByte(lcd, 0, byte);
+}
+
+void sendData(struct Lcd *lcd, int byte){
+	sendByte(lcd, 1, byte);
+}
+
+void clearLcd(struct Lcd *lcd){
+	sendCommand(lcd, 1);
+	usleep(164 * 2 * 10);
+}
+
+void gotoLine(struct Lcd *lcd, int nextLine){
+	const int SET_LINE = 0b10000000;
+	switch(nextLine){
+		case 0:
+			sendCommand(lcd, SET_LINE | 0);
+			return;
+		case 1:
+			sendCommand(lcd, SET_LINE | 0x40);	
+			return;
+		default:
+			return;
+		// todo: implement case2, case3
+	}
+}
+
+void printLcd(struct Lcd *lcd, char *text){
+	int i = 0;
+	int line = 0;
+	while(1){
+		// avoid forever loop
+		if(*(text + i) == '\0' || i > 0xffff){
+			return;
+		}
+		if(*(text + i) == '\n'){
+			gotoLine(lcd, ++line);
+		} else {
+			sendData(lcd, *(text + i));
+		}
+		i++;
+	}
+}
+
+void initDisplay(struct Lcd *lcd)
 {
 	usleep(40 * 1000);
-	sendNibble(pin, 0, 0, 0, 1, 1);
+	sendNibble(lcd, 0, 0, 0, 1, 1);
 	usleep(8 * 1000);
-	sendNibble(pin, 0, 0, 0, 1, 1);
+	sendNibble(lcd, 0, 0, 0, 1, 1);
 	usleep(200);
-	sendNibble(pin, 0, 0, 0, 1, 1); // detarmined 8-bit mode
-	sendNibble(pin, 0, 0, 0, 1, 0); // set to 4-bit mode
-	sendByte(pin, 0, 0b00101000); // function set
-	sendByte(pin, 0, 0b00001100); // turn on display
-	sendByte(pin, 0, 1); // clear display
-	usleep(164 * 2 * 10);
-	sendByte(pin, 0, 0b00000110); // set entry mode
+	sendNibble(lcd, 0, 0, 0, 1, 1); // detarmined 8-bit mode
+	sendNibble(lcd, 0, 0, 0, 1, 0); // set to 4-bit mode
+	sendCommand(lcd, 0b00101000); // function set
+	sendCommand(lcd, 0b00001100); // turn on display
+	clearLcd(lcd);
+	sendCommand(lcd,  0b00000110); // set entry mode
 }
