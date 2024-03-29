7a40770e6ac67b0d8fcbf7854e6f8c1bc54e3494
Sola <terakou1105@gmail.com>
2024-03-30 00:38:54 +0900

    Fix segmentation fault

--- lib44780/test.c
+++ lib44780/test.c
@@ -4,10 +4,11 @@
 
 int main(int argc, char **argv)
 {
-	struct Lcd *lcd;
-	openLcd(lcd, "gpiochip0", 7, 8, 25, 24, 23, 18);
-	initLcd(lcd);
+	struct Lcd lcd;
+	openLcd(&lcd, "gpiochip0", 7, 8, 25, 24, 23, 18);
+	initLcd(&lcd);
 	char txt[32] = "Hello World!\nHD44780 Char LCD";	
-	closeLcd(lcd);
+	printLcd(&lcd, txt);
+	closeLcd(&lcd);
 	return 0;
 }
